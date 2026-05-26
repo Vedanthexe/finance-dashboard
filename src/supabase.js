311bb7f30afd673802ed94c47d4cd2c4ab27d6a4
Vedanth Rohit <vedanth@Vedanths-Macbook-Pro-2.local>
2026-05-26 18:09:40 +0530

    hardcode supabase keys

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+const supabaseUrl = 'https://vgqqtmoqjeknojdbirsh.supabase.co'
+const supabaseAnonKey = 'sb_publishable_EUVRVxKMLkgfaVu6H454ew_SXniGLs7'
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
