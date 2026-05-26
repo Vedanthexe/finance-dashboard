3b09c336274eb5f4bf67585ce5a84cdacf673102
Vedanth Rohit <vedanth@Vedanths-Macbook-Pro-2.local>
2026-05-26 18:15:58 +0530

    fix supabase key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://vgqqtmoqjeknojdbirsh.supabase.co'
-const supabaseAnonKey = 'sb_publishable_EUVRVxKMLkgfaVu6H454ew_SXniGLs7'
-
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZncXF0bW9xamVrbm9qZGJpcnNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3NzY1NTcsImV4cCI6MjA5NTM1MjU1N30.o7s9Mh8uMzAomY6GhJQh24iXGwxziWSv20DBoJYC8es'
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
